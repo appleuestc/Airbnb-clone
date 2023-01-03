782cc6a286c2032f18e25da2d8df5f033fa61390
Anbo <1801380172@qq.com>
2023-01-03 12:08:21 -0800

    modified format

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   images:{
-    domains:['links.papareact.com'],
+    domains:["links.papareact.com"],
   },
   env: {
-    mapbox_key: 'pk.eyJ1IjoiYXBwbGV1Y3NkIiwiYSI6ImNsY2ZpdjZ2azIzNGQzcG4yYXo3NWJsNjYifQ.pULvjtICSgki5B_X2ygeHA',
+    mapbox_key: "pk.eyJ1IjoiYXBwbGV1Y3NkIiwiYSI6ImNsY2ZpdjZ2azIzNGQzcG4yYXo3NWJsNjYifQ.pULvjtICSgki5B_X2ygeHA",
   },
 }
